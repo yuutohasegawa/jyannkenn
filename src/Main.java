a39dd13979574e6cb76982eb15e24e5160afac00
Yuuto Hasegawa <146802468+yuutohasegawa@users.noreply.github.com>
2024-07-12 12:29:19 +0900

    CPUの手をランダムにする処理を実装した close #15 close #16

--- src/Main.java
+++ src/Main.java
@@ -92,13 +92,13 @@ public class Main {
 
 
 
-        //以降にテストコードを追加して検証を行う
-//以降にテストコードを追加して検証を行う
+//comの手を生成
 
-        System.out.println("テスト結果");
+        com.createRandomHand();
 
-        System.out.println(user.getHand().getHandName());
+        System.out.println("ランダムなジャンケンの手をCPUにセットする");
 
+        System.out.println(com.getHand().getHandName());
 
     }
 
