fa0d79fbe0566128357769fc9a1c1d3758e63f2e
Yuuto Hasegawa <146802468+yuutohasegawa@users.noreply.github.com>
2024-07-12 11:48:51 +0900

    ユーザー入力で手を決める close #10,

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 package src;
 
+import java.io.*;
+
+
+
 public class Main {
+
 
 
     public static void main(String[] args) {
@@ -8,6 +13,7 @@ public class Main {
         Player user = new Player("ユーザ");
 
         Player com = new Player("CPU");
+
 
 
         //入力指示表示
@@ -19,10 +25,81 @@ public class Main {
         System.out.println("3.パー");
 
         System.out.println("あなたの手を選択してください。>");
-        //テストコード
-        System.out.println(user.getPlayerName());
-        com.setPlayerName("コンピュータ");
-        System.out.println(com.getPlayerName());
+
+
+
+        //ユーザ入力
+
+        String input = null;
+
+        try {
+
+            BufferedReader inputuser = new BufferedReader(
+
+                    new InputStreamReader(System.in)
+
+            );
+
+            input = inputuser.readLine();
+
+        } catch (IOException e) {
+
+            System.out.print("システムエラー");
+
+            System.exit(0);
+
+            return;
+
+        }
+
+
+
+        if (input == null) {
+
+            System.out.print("1-3の値を入力してください。");
+
+            System.exit(0);
+
+            return;
+
+        }
+
+
+
+        //ユーザの入力によりジャンケンの手をセットする
+
+        if (input.equals("1")) {
+
+            user.setHand(new Gu());
+
+        } else if (input.equals("2")) {
+
+            user.setHand(new Choki());
+
+        } else if (input.equals("3")) {
+
+            user.setHand(new Pa());
+
+        } else {
+
+            System.out.print("1-3の値を入力してください。");
+
+            System.exit(0);
+
+            return;
+
+        }
+
+
+
+        //以降にテストコードを追加して検証を行う
+//以降にテストコードを追加して検証を行う
+
+        System.out.println("テスト結果");
+
+        System.out.println(user.getHand().getHandName());
+
+
     }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -4,6 +4,10 @@ public class Player {
 
     private String playerName;
 
+    private Hand hand;
+
+
+
     public Player(String playerName) {
 
         this.playerName = playerName;
@@ -19,6 +23,22 @@ public class Player {
     public void setPlayerName(String playerName) {
 
         this.playerName = playerName;
+
+    }
+
+
+
+    public Hand getHand() {
+
+        return this.hand;
+
+    }
+
+
+
+    public void setHand(Hand hand) {
+
+        this.hand = hand;
 
     }
 
